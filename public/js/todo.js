170a75a8de110bbe1ff26aca270cf27865bdfc51
Mason Kline <mkline13@gmail.com>
2024-02-01 01:05:09 -0800

    now uses <x> tags for templating

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,42 +1,49 @@
-function compileTemplate(templateNode) {
-    // build a map of the name of template fields to functions that will fill them
-    const templateFieldNodes = templateNode.content.querySelectorAll('[data-field]');
-    const mapping = {};
-    for (const node of templateFieldNodes) {
-        const key = node.dataset.field;
-        if (key === 'id') {
-            throw new Error("cannot use 'id' as a template field name")
-        }
-        const value = (value) => {
-            const nodes = templateFieldNodes;
-            const nodePtr = node;
-            nodePtr.innerText = value;
-        };
-        mapping[key] = value;
-        node.removeAttribute('data-field'); // remove the data-field attribute so markup is cleaner
+
+
+function select(element, path) {
+    const snapshot = document.evaluate(
+        path,
+        element,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    );
+
+    const result = [];
+
+    for (let i = 0; i < snapshot.snapshotLength; i++) {
+        result.push(snapshot.snapshotItem(i));
     }
 
-    // create the function for rendering the template
-    const render = (data) => {
-        for (const k in mapping) {
-            mapping[k](data[k]);
-        }
-        const orig = templateNode.content;
-        const clone = orig.cloneNode(true);
-        return clone;
-    };
-
-    return render;
+    return result;
 }
 
-function gatherAndCompileTemplates(parentNode) {
-    // returns an object mapping the ids of template nodes to functions for rendering them
-    const templateNodes = parentNode.getElementsByTagName('template');
-    const map = {};
+function renderTemplate(templateElement, parent, data) {
+    // make a clone of a template node
+    const clone = templateElement.content.cloneNode(true);
+    const fields = clone.querySelectorAll('x');
 
-    for (const n of templateNodes) {
-        map[n.id] = compileTemplate(n);
+    parent.appendChild(clone);
+
+    // replace fields in clone with text in the data object
+    for (const node of fields) {
+        const key = node.getAttribute('name');
+        const value = data[key] ?? undefined;
+
+        if (value !== undefined ) {
+            node.replaceWith(value);
+        }
+        else {
+            const path = node.getAttribute('path');
+            if (path) {
+                const victims = select(node, path);
+                for (const v of victims) {
+                    v.remove();
+                }
+            }
+            else {
+                node.remove();
+            }
+        }
     }
-
-    return map;
 }
